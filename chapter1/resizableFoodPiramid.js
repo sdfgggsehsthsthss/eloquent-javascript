6a37485e92642cd7d9534847f8380c3bc14347dc
Felipe B. Hummel <47154424+sdfgggsehsthsthss@users.noreply.github.com>
2025-03-14 17:43:44 -0300

    Create resizableFoodPiramid.js

---
+++ chapter1/resizableFoodPiramid.js
@@ -0,0 +1,5 @@
+console.log(arroz)
+for(var arroz = `comida`, counter = 0 , size = Number(prompt(`size`)); counter <= size; counter++, arroz=`|${arroz}`, arroz=`${arroz}|` ){
+  console.log(`${` `.repeat(12 - counter)}${arroz}` )
+}
+console.log(arroz);
