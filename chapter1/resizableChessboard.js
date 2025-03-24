f0c94128a27be593f757957effb372c1cc4c8e61
Felipe B. Hummel <47154424+sdfgggsehsthsthss@users.noreply.github.com>
2025-03-24 18:47:53 -0300

    Update resizableChessboard.js

--- chapter1/resizableChessboard.js
+++ chapter1/resizableChessboard.js
@@ -2,7 +2,7 @@ var sizeX = Number(prompt(`sizeX`));
 var sizeY	= Number(prompt(`sizeY`));
 var counter = 0;
 
-for (let vazio = ` `, hash = `#` , axisY = hash + vazio,axisX = vazio + hash;counter <= sizeY ; counter++){
+for (let vazio = ` `, hash = `#` , axisY = hash + vazio,axisX = vazio + hash;counter < sizeY ; counter++){
     if(counter % 2 ==0){
       console.log(axisY.repeat(sizeX));
     }else
